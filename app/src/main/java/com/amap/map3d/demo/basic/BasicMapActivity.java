25392cef93207bc6cdab436dc909f089dfa837e9
tangchao <419704299@qq.com>
2018-03-12 10:03:45 +0800

    logger日志

--- app/src/main/java/com/amap/map3d/demo/basic/BasicMapActivity.java
+++ app/src/main/java/com/amap/map3d/demo/basic/BasicMapActivity.java
@@ -14,9 +14,11 @@ import com.amap.api.maps.AMap.CancelableCallback;
 import com.amap.api.maps.AMap.OnMapClickListener;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.MapsInitializer;
 import com.amap.api.maps.model.CameraPosition;
 import com.amap.api.maps.model.LatLng;
 import com.amap.map3d.demo.R;
+import com.amap.map3d.demo.util.OffLineMapUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -48,7 +50,7 @@ public class BasicMapActivity extends Activity implements OnClickListener{
          * 则需要在离线地图下载和使用地图页面都进行路径设置
          * */
 		//Demo中为了其他界面可以使用下载的离线地图，使用默认位置存储，屏蔽了自定义设置
-		//  MapsInitializer.sdcardDir =OffLineMapUtils.getSdCacheDir(this);
+//		  MapsInitializer.sdcardDir = OffLineMapUtils.getSdCacheDir(this);
 
 		mapView = (MapView) findViewById(R.id.map);
 		mapView.onCreate(savedInstanceState);// 此方法必须重写
